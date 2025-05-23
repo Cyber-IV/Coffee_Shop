87a933944cac7b2a85a55de9f43b97042fea69f4
Cyber <cyber.voyager@outlook.com>
2025-05-23 17:55:53 -0600

    createElement

--- js/script.js
+++ js/script.js
@@ -7,4 +7,13 @@ const links = document.querySelectorAll('a');
 links[0].title = 'Regresar a Homepage';
 links[0].classList.add('home_icon-tooltip');
 links[1].textContent = 'Sobre Nosotros';
-// getElementById
+
+// createElement
+const newLink = document.createElement('A');
+// setting attribuites
+newLink.href = 'advices.html';
+newLink.textContent = 'Consejos';
+newLink.classList.add('nav__link');
+// adding the link to navigation
+const navigation = document.querySelector('.navigation');
+navigation.appendChild(newLink);
