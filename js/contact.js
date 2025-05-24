b8a89fa896ef480b1f0c65ff8dba24ce188be84f
Cyber <cyber.voyager@outlook.com>
2025-05-23 23:41:42 -0600

    Click Events

---
+++ js/contact.js
@@ -0,0 +1,8 @@
+// Eventos click
+const sendData_btn = document.querySelector('.contact-button');
+sendData_btn.addEventListener('click', (e) => {
+    console.log(e); //Muestra la informacion del evento
+    e.preventDefault(); // Evita que el formulario se envíe y recargue la página por defecto
+
+    // Validar el formulario
+});
