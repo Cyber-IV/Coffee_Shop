40cbfb8238a601ef04442a4ca1569c3c2c6bc04b
Cyber <cyber.voyager@outlook.com>
2025-05-25 03:20:52 -0600

    Correcta organizacion y Submit de formulario Preparacion para validacion de formulario y despues su envio

--- js/contact.js
+++ js/contact.js
@@ -1,33 +1,48 @@
 // Eventos click
-const sendData_btn = document.querySelector('.contact-button');
+/* const sendData_btn = document.querySelector('.contact-button');
 sendData_btn.addEventListener('click', (e) => {
     console.log(e); //Muestra la informacion del evento
     e.preventDefault(); // Evita que el formulario se envíe y recargue la página por defecto
+}); */
 
-    // Validar el formulario
-});
-
+// Eventos de inputs y text areas - Variables en 1ra posicion
 const inputed_data = {
     name: '',
     email: '',
     message: '',
 }
-// Eventos de inputs y text areas
 const name_input = document.querySelector('#name');
 const email_input = document.querySelector('#email');
 const message_input = document.querySelector('#message');
+const form = document.querySelector('.contact-form');
 /* name_input.addEventListener('change', (e) => { // Change - Detecta cambios en el input cuando pierde el foco
     console.log('se escribio algo en nombre...');
 }); */
 /* name_input.addEventListener('input', (e) => { // Input - Detecta cambios en el input cuando se escribe algo
     console.log(e.target.value); // e - Es el evento que sucede / target - El objetivo del evento / value - El valor del input
 }); */
+// Event listeners en 2da posicion
 name_input.addEventListener('change', saveDataInput);
 email_input.addEventListener('change', saveDataInput);
 message_input.addEventListener('change', saveDataInput);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
+    // Validar formulario
+
+    // Enviar formulario
+});
+
+// Funciones que se necesiten en 3ra posicion
 function saveDataInput(e){
     // Es importante que los id de los inputs y las propiedades del objeto donde se guardaran
     inputed_data[e.target.id] = e.target.value;
     console.log(inputed_data);
+}
+
+function validateForm(){
+
+}
+function sendForm(){
+
 }
